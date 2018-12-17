8ae0957091c4fb12944a89cd276258ecfeb109b3
tomrf1 <tom.forbes@theguardian.com>
2018-12-17 10:55:19 +0000

    support running locally

--- src/main/scala/com/gu/ophan/AcquisitionService.scala
+++ src/main/scala/com/gu/ophan/AcquisitionService.scala
@@ -1,8 +1,11 @@
 package com.gu.ophan
 
-import com.gu.acquisition.services.LambdaConfig
+import com.amazonaws.auth.{AWSCredentialsProviderChain, InstanceProfileCredentialsProvider}
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.gu.acquisition.services.{DefaultAcquisitionServiceConfig, Ec2OrLocalConfig, LambdaConfig}
 import com.gu.config.Configuration
 import com.gu.okhttp.RequestRunners
+import com.gu.support.config.Stages
 
 object AcquisitionService {
 
@@ -11,9 +14,18 @@ object AcquisitionService {
       com.gu.acquisition.services.MockAcquisitionService
     } else {
 
-      val config = LambdaConfig(
-        kinesisStreamName = Configuration.kinesisStreamName
-      )
+      //Credentials provider is only required if running locally
+      val config: DefaultAcquisitionServiceConfig = {
+        if (Configuration.stage == Stages.DEV) {
+          val credentialsProvider = new AWSCredentialsProviderChain(
+            new ProfileCredentialsProvider("membership"),
+            InstanceProfileCredentialsProvider.getInstance()
+          )
+
+          Ec2OrLocalConfig(credentialsProvider, Configuration.kinesisStreamName)
+        } else LambdaConfig(Configuration.kinesisStreamName)
+      }
+
       com.gu.acquisition.services.AcquisitionService.prod(config)(RequestRunners.client)
     }
 }
